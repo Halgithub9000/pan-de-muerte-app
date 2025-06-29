8f39c02ad44bc146c34757a4dae7c992c8636716
csverinogpg <csverinogpg@gmail.com>
2025-06-29 14:58:53 -0400

    feat:add config for apk building

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import HomeScreen from './app/(tabs)/index';
 
 export default function App() {
-  return <HomeScreen />;
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        // Mantén el splash visible
+        await SplashScreen.preventAutoHideAsync();
+        // Simula un retraso de 2 segundos
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Oculta el splash
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepare();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <HomeScreen />
+    </View>
+  );
 }
