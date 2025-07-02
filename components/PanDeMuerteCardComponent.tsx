9207d472bd0590dad4bcea155a1dec691dfba75c
csverinogpg <csverinogpg@gmail.com>
2025-07-01 20:15:18 -0400

    feat:get a different quote every time the app is started

--- components/PanDeMuerteCardComponent.tsx
+++ components/PanDeMuerteCardComponent.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import PanDeMuerteCardComponentStyles from '../styles/PanDeMuerteCardComponentStyles';
-
-interface Quote {
-  cita: string;
-  autor: string;
-  lugar?: string; 
-}
+import { Quote } from '../types/Quote';
 
 const PanDeMuerteCardComponent = ({ quote }: { quote: Quote }) => {
 
--- components/PanDeMuerteComponent.tsx
+++ components/PanDeMuerteComponent.tsx
@@ -1,36 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import quotes from '../data/citas.json'; // Carga las citas desde el archivo JSON
-import PanDeMuerteCardComponent from './PanDeMuerteCardComponent'; // Importa el componente CardComponent
+import quotes from '../data/citas.json';
+import PanDeMuerteCardComponent from './PanDeMuerteCardComponent';
+import { Quote } from '../types/Quote';
 
-const getDailyQuote = async () => {
-  const today = new Date().toISOString().split('T')[0]; // Obtén la fecha actual (YYYY-MM-DD)
-  const storedDate = await AsyncStorage.getItem('lastDate');
-  const storedQuote = await AsyncStorage.getItem('dailyQuote');
-
-  if (storedDate === today && storedQuote) {
-    // Si la fecha no ha cambiado, devuelve la cita almacenada
-    return JSON.parse(storedQuote);
-  } else {
-    // Si la fecha ha cambiado, selecciona una nueva cita
-    const index = Math.floor(Math.random() * quotes.length);
-    const newQuote = quotes[index];
-
-    // Guarda la nueva cita y la fecha
-    await AsyncStorage.setItem('lastDate', today);
-    await AsyncStorage.setItem('dailyQuote', JSON.stringify(newQuote));
-
-    return newQuote;
-  }
+const getDailyQuote = (): Quote => {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
 };
 
 const PanDeMuerteComponent = () => {
-  const [quote, setQuote] = useState(null);
+  const [quote, setQuote] = useState<Quote | null>(null);
 
   useEffect(() => {
-    const fetchQuote = async () => {
-      const dailyQuote = await getDailyQuote();
+    const fetchQuote = () => {
+      const dailyQuote = getDailyQuote();
       setQuote(dailyQuote);
     };
 
