0bd87e27d9954983ad830ef00e4eca058613c877
csverinogpg <csverinogpg@gmail.com>
2025-07-02 23:34:53 -0400

    feat: set fixed size card and minimize font when quote doesnt fit to card

--- components/PanDeMuerteCardComponent.tsx
+++ components/PanDeMuerteCardComponent.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import { Quote } from '../types/Quote';
+import { RFValue } from 'react-native-responsive-fontsize'; // Importa la función para tamaños dinámicos
 
 const PanDeMuerteCardComponent = ({ quote }: { quote: Quote }) => {
-
   return (
     <View style={styles.card}>
-      <Text style={styles.quoteText}>{quote.cita}</Text>
+      <Text 
+      numberOfLines={4}
+      adjustsFontSizeToFit
+      style={styles.quoteText}>{quote.cita}</Text>
       <Text style={styles.authorText}>- {quote.autor}</Text>
     </View>
   );
@@ -14,21 +17,24 @@ const PanDeMuerteCardComponent = ({ quote }: { quote: Quote }) => {
 
 const styles = StyleSheet.create({
   card: {
+    width: '90%', // Responsivo: ocupa el 90% del ancho de la pantalla
+    height: '20%', // Altura porcentual: ocupa el 30% de la pantalla
     padding: 20,
     backgroundColor: '#1e1e1e',
     borderRadius: 5,
     alignItems: 'center',
-    marginHorizontal: 20,
+    justifyContent: 'center', // Centra el contenido verticalmente
+    marginHorizontal: '5%', // Centra la card horizontalmente
   },
   quoteText: {
-    fontSize: 18,
+    fontSize: RFValue(18, Dimensions.get('window').height), // Ajusta dinámicamente el tamaño de la fuente
     color: '#ffffff',
     textAlign: 'center',
     fontFamily: 'sans-serif-condensed',
     marginBottom: 10,
   },
   authorText: {
-    fontSize: 16,
+    fontSize: RFValue(16, Dimensions.get('window').height), // Ajusta dinámicamente el tamaño de la fuente
     color: '#ffffff',
     textAlign: 'center',
     fontFamily: 'sans-serif-condensed',
